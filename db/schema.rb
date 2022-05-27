eeb1931e37b3b09cf1635d7337ebba9463756b8a
Hiroto Ohno <a2206ho@aiit.ac.jp>
2022-05-27 12:54:25 +0000

    ch 13.5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_19_151732) do
+ActiveRecord::Schema.define(version: 2022_05_24_143547) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,4 +30,5 @@ ActiveRecord::Schema.define(version: 2022_05_19_151732) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "microposts", "users"
 end
