468e447aa0da71b347d6821fd929b7bff24d0e9b
Hiroto Ohno <a2206ho@aiit.ac.jp>
2022-05-30 16:03:19 +0000

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ User.create!(name:  "Example User",
                email: email,
                password:              password,
                password_confirmation: password)
-               
+end
                
 # ユーザーの一部を対象にマイクロポストを生成する
 users = User.order(:created_at).take(6)
@@ -22,4 +22,11 @@ users = User.order(:created_at).take(6)
   content = Faker::Lorem.sentence(word_count: 5)
   users.each { |user| user.microposts.create!(content: content) }
 end             
-end
+
+# 以下のリレーションシップを作成する
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
